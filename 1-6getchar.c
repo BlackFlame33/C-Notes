1db361eb1422cc490609f9fcaebca1eb125dc227
BlackFlame33 <blackflame33@icloud.com>
2020-03-09 23:13:26 +0800

    TO-DO:单元测试

--- 1-6getchar.c
+++ 1-6getchar.c
@@ -155,7 +155,7 @@ int main()
     }
 
     return 0;
-}//主要要注意的是：1、全等。2、打印时'\'需要用'\\'*/
+}//主要要注意的是：1、全等。2、打印时'\'需要用'\\'
 
 #include <stdio.h>
 #define IN 1    //在单词内
@@ -179,6 +179,82 @@ int main(){
         }
     }
     printf("nl:%d\nnw:%d\nnc:%d\n", nl, nw, nc);
+
+    return 0;
+}*/
+
+//练习1-11  测试单词计数程序
+//TO-DO:单元测试听说过，但还不大会，先贴上官方答案，待学会后再回来查看！！！
+#include <assert.h>
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *f;
+    unsigned long i;
+    static char *ws = " \f\t\v";
+    static char *al = "abcdefghijklmnopqrstuvwxyz";
+    static char *i5 = "a b c d e f g h i j k l m "
+                      "n o p q r s t u v w x y z "
+                      "a b c d e f g h i j k l m "
+                      "n o p q r s t u v w x y z "
+                      "a b c d e f g h i j k l m "
+                      "n\n";
+
+    /* Generate the following: */
+    /* 0. input file contains zero words */
+    f = fopen("test0", "w");
+    assert(f != NULL);
+    fclose(f);
+
+    /* 1. input file contains 1 enormous word without any newlines */
+    f = fopen("test1", "w");
+    assert(f != NULL);
+    for (i = 0; i < ((66000ul / 26) + 1); i++)
+        fputs(al, f);
+    fclose(f);
+
+    /* 2. input file contains all white space without newlines */
+    f = fopen("test2", "w");
+    assert(f != NULL);
+    for (i = 0; i < ((66000ul / 4) + 1); i++)
+        fputs(ws, f);
+    fclose(f);
+
+    /* 3. input file contains 66000 newlines */
+    f = fopen("test3", "w");
+    assert(f != NULL);
+    for (i = 0; i < 66000; i++)
+        fputc('\n', f);
+    fclose(f);
+
+    /* 4. input file contains word/ 
+     *    {huge sequence of whitespace of different kinds}
+     *    /word 
+     */
+    f = fopen("test4", "w");
+    assert(f != NULL);
+    fputs("word", f);
+    for (i = 0; i < ((66000ul / 26) + 1); i++)
+        fputs(ws, f);
+    fputs("word", f);
+    fclose(f);
+
+    /* 5. input file contains 66000 single letter words,
+     *    66 to the line 
+     */
+    f = fopen("test5", "w");
+    assert(f != NULL);
+    for (i = 0; i < 1000; i++)
+        fputs(i5, f);
+    fclose(f);
+
+    /* 6. input file contains 66000 words without any newlines */
+    f = fopen("test6", "w");
+    assert(f != NULL);
+    for (i = 0; i < 66000; i++)
+        fputs("word ", f);
+    fclose(f);
 
     return 0;
 }
