af1853dc617b99a10fb825eacb8dc984daa8969a
BlackFlame33 <blackflame33@icloud.com>
2020-03-13 21:28:52 +0800

    getline是既定函数，while适合不明确循环步数的情况

--- 1-15function.c
+++ 1-15function.c
@@ -17,7 +17,7 @@ int power(int base, int n)
         p = p * base;
 
     return p;
-}*/
+}
 
 //练习1-15  使用函数实现温度转换计算
 #include <stdio.h>
@@ -36,4 +36,61 @@ double temperature(int F)
     C = (5 / 9.0) * (F - 32);
 
     return C;
+}*/
+
+//读入一组文本，并把最长的文本行打印出来。
+#include <stdio.h>
+#define MAXLINE 1000 //允许的输入行的最大长度
+
+int getlines(char line[], int maxline);
+void copy(char to[], char from[]);
+//打印最长的输入行
+int main()
+{
+    int len;               //当前行长度
+    int max;               //目前为止发现的最长行的长度
+    char line[MAXLINE];    //当前的输入行
+    char longest[MAXLINE]; //用于保存最长的行
+
+    max = 0;
+    while ((len = getlines(line, MAXLINE)) > 0)
+    {
+        if (len > max)
+        {
+            max = len;
+            copy(longest, line);
+        }
+    }
+    if (max > 0) //存在这样的行
+        printf("%s", longest);
+
+    return 0;
+}
+
+//getlines函数：将一行读入s中并返回其长度
+int getlines(char s[], int lim)
+{
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+
+    return i;
+}
+
+//copy函数：将from复制到to；假定to足够大
+void copy(char to[], char from[])
+{
+    int i = 0;
+    while ((to[i] = from[i]) != '\0')
+    /*为什么要用while，不用for？
+    因为在这个循环操作中，from的下标是未知的，也就是说不清楚循环的步数。
+    使用while可以设定为当复制的当前字符为EOF的时候便可结束函数。
+    */
+        ++i;
 }
