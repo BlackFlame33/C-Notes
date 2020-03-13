e59b5734d97bb0c1a28749299bdfe7803b22969d
BlackFlame33 <blackflame33@icloud.com>
2020-03-13 16:56:36 +0800

    完成练习1-15

--- 1-15function.c
+++ 1-15function.c
@@ -1,4 +1,4 @@
-//定义一个power函数用于计算整数的n次幂
+/*//定义一个power函数用于计算整数的n次幂
 #include <stdio.h>
 int power(int m, int n);
 int main()
@@ -17,4 +17,23 @@ int power(int base, int n)
         p = p * base;
 
     return p;
+}*/
+
+//练习1-15  使用函数实现温度转换计算
+#include <stdio.h>
+double temperature(int F);
+int main()
+{
+    printf("华氏度——>摄氏度\n");
+    for (int F; F <= 300; F += 20)
+        printf("%3d\t%6.2f\n\n", F, temperature(F));
+
+    return 0;
+}
+double temperature(int F)
+{
+    double C;
+    C = (5 / 9.0) * (F - 32);
+
+    return C;
 }
